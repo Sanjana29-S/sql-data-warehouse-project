afb7846a5bebc0a32d07068903d9fdde61914adb
Sanjana Singh <70069746+Sanjana29-S@users.noreply.github.com>
2025-11-11 11:53:58 +0530

    Refactor crm_cust_info and crm_sales_details tables

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -12,14 +12,14 @@ Script Purpose:
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info(
-	cst_id INT,
-	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
+	cst_id             INT,
+	cst_key            NVARCHAR(50),
+	cst_firstname      NVARCHAR(50),
+	cst_lastname 	   NVARCHAR(50),
 	cst_marital_status NVARCHAR(50),
-	cst_gender NVARCHAR(50),
-	cst_create_date DATE,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	cst_gender 		   NVARCHAR(50),
+	cst_create_date    DATE,
+	dwh_create_date    DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
@@ -39,15 +39,15 @@ CREATE TABLE silver.crm_prd_info(
 IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details(
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT,
+	sls_ord_num  	NVARCHAR(50),
+	sls_prd_key	    NVARCHAR(50),
+	sls_cust_id 	INT,
+	sls_order_dt 	DATE,
+	sls_ship_dt  	DATE,
+	sls_due_dt 		DATE,
+	sls_sales		INT,
+	sls_quantity 	INT,
+	sls_price 		INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
